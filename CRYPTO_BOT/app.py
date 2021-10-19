0726251f5ed3de6312e13610cbec9047eb59159f
jahsayd <37016563+jahsayd@users.noreply.github.com>
2021-10-19 11:30:48 +0300

    Bot for telegram with virtual len, all files

---
+++ CRYPTO_BOT/app.py
@@ -0,0 +1,39 @@
+import telebot
+from config import keys, TOKEN
+from extension import ConvertionException, CryptoConventer
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в формате:' \
+           '\n<имя валюты>\<в какую валюту перевести>\
+           <количество переводимой валюты>\nУвидеть список доступных вылют: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные монеты:\n'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text'])
+def convert(message: telebot.types.Message):
+    try:
+        value = message.text.split(' ')
+        if len(value) != 3:
+            raise ConvertionException('Слишком много параметров')
+
+        quote, base, amount = value
+        total_base = CryptoConventer.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}' )
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ CRYPTO_BOT/config.py
@@ -0,0 +1,9 @@
+TOKEN = '2073748249:AAHSxhYtIb7vC11IWzi_AobKmAUK9layfsY'
+
+keys = {
+    'bitcoin': 'BTC',
+    'etherium': 'ETH',
+    'dollar': 'USD',
+    'euro': 'EUR',
+    'ruble': 'RUB'
+}
