6e810f1a05f290c25026fea1a643a919792fb4a4
jahsayd <37016563+jahsayd@users.noreply.github.com>
2022-01-29 16:45:20 +0300

    Changes for News Portal

--- news_portal/NewsPaper/news/views.py
+++ news_portal/NewsPaper/news/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы
+    # обратиться к самому списку объектов через HTML-шаблон
+
+
+# создаём представление, в котором будут детали конкретного отдельного поста
+class PostDetail(DetailView):
+    model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'new.html'  # название шаблона будет product.html
+    context_object_name = 'new'
